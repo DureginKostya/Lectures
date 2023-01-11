b699ffa12607a247d09a29af89f18f56528e2334
DureginKostya <kot-502@mail.ru>
2023-01-11 12:09:05 +0500

    Added folder Example013_ArrangeMassiv

---
+++ Lesson_3/Examples/Example013_ArrangeMassiv/Program.cs
@@ -0,0 +1,36 @@
+﻿//Упорядочить массив по возрастанию.
+Console.Clear();
+int[] Massiv = {6, 8, 3, 2, 1, 4, 5, 1, 1};
+void PrintMassiv(int[] Mas)
+{
+    int count = Mas.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(Mas[i]);
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
+int[] GetMassiv (int[] Array)
+{
+    int count = Array.Length;
+   // int MinPosition = 0;
+    for (int i = 0; i < count - 1; i++)
+    {
+       // int MinVariable = Array[i];
+        for (int j = i + 1; j < count; j++)
+        {
+            if (Array[j] < Array[i])
+            {
+                int key = Array[i];
+                Array[i] = Array[j];
+                Array[j] = key;
+            }
+        }
+
+    }
+    return Array;
+}
+PrintMassiv(Massiv);
+int[] ArrangeMassiv = GetMassiv(Massiv);
+PrintMassiv(ArrangeMassiv);
